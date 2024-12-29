15646f3d2b69f3144a827474b30b2391cfb41516
MonopolyBotTelegram <monopolybottelegram@gmail.com>
2024-12-29 10:13:40 -0500

    Add files via upload

--- scripts/MINEROS.js
+++ scripts/MINEROS.js
@@ -13,35 +13,35 @@ return cellEstrellasByNumber[y][x];
 }
 
 
-function MINEROS_load(){
+async function MINEROS_load(){
 //////////////////
-const mineros=getMinerosM();
+const mineros=await getMinerosM();
 /////////////////
 for(let y=0;y<6;y++){
 for(let x=0;x<5;x++){
 if(y===0){
-if(x===1){cellEstrellasByNumber[y][x]=mineros.substring(0,1).toNumber;}
-if(x===3){cellEstrellasByNumber[y][x]=mineros.substring(1,2).toNumber;}
+if(x===1){cellEstrellasByNumber[y][x]=Number(mineros.substring(4,5));}//ok
+if(x===3){cellEstrellasByNumber[y][x]=Number(mineros.substring(5,6));}//ok
 }
 if(y===1){
-if(x===0){cellEstrellasByNumber[y][x]=mineros.substring(2,3).toNumber;}
-if(x===4){cellEstrellasByNumber[y][x]=mineros.substring(3,4).toNumber;}
+if(x===0){cellEstrellasByNumber[y][x]=Number(mineros.substring(3,4));}//ok
+if(x===4){cellEstrellasByNumber[y][x]=Number(mineros.substring(6,7));}
 }
 if(y===2){
-if(x===0){cellEstrellasByNumber[y][x]=mineros.substring(4,5).toNumber;}
-if(x===4){cellEstrellasByNumber[y][x]=mineros.substring(5,6).toNumber;}
+if(x===0){cellEstrellasByNumber[y][x]=Number(mineros.substring(2,3));}//ok
+if(x===4){cellEstrellasByNumber[y][x]=Number(mineros.substring(7,8));}
 }
 if(y===3){
-if(x===0){cellEstrellasByNumber[y][x]=mineros.substring(6,7).toNumber;}
-if(x===4){cellEstrellasByNumber[y][x]=mineros.substring(7,8).toNumber;}
+if(x===0){cellEstrellasByNumber[y][x]=Number(mineros.substring(1,2));}//0k
+if(x===4){cellEstrellasByNumber[y][x]=Number(mineros.substring(8,9));}
 }
 if(y===4){
-if(x===0){cellEstrellasByNumber[y][x]=mineros.substring(8,9).toNumber;}
-if(x===4){cellEstrellasByNumber[y][x]=mineros.substring(9,10).toNumber;}
+if(x===0){cellEstrellasByNumber[y][x]=Number(mineros.substring(0,1));}//ok
+if(x===4){cellEstrellasByNumber[y][x]=Number(mineros.substring(9,10));}
 }
 if(y===5){
-if(x===1){cellEstrellasByNumber[y][x]=mineros.substring(10,11).toNumber;}
-if(x===3){cellEstrellasByNumber[y][x]=mineros.substring(11,12).toNumber;}
+if(x===1){cellEstrellasByNumber[y][x]=Number(mineros.substring(11,12));}
+if(x===3){cellEstrellasByNumber[y][x]=Number(mineros.substring(10,11));}
 }
 }
 }
@@ -50,11 +50,11 @@ if(x===3){cellEstrellasByNumber[y][x]=mineros.substring(11,12).toNumber;}
 
 
 async function getMinerosM(){
-const url=`${CONSTANTES_url_get_mineros()}?action=getMineros&char_id=${VARIABLES_get_chatId()}&password=${VARIABLES_get_password()}`;
+const url=`https://cors-anywhere.herokuapp.com/${CONSTANTES_url_get_mineros()}?action=getMineros&char_id=${VARIABLES_get_chatId()}&password=${VARIABLES_get_password()}`;
 let intentos=0;
 let mineros;
 while(true){
-mineros=load_url_mineros(url);
+mineros=await load_url_mineros(url);
 if(mineros===null){
 intentos++;
 if(intentos===10){break}
--- scripts/VARIABLES.js
+++ scripts/VARIABLES.js
@@ -6,6 +6,6 @@ function VARIABLES_get_chatId(){return chatId;}
 function VARIABLES_get_walletAddress(){return walletAddress;}
 function VARIABLES_get_password(){return password;}
 
-function VARIABLES_set_chatId(p){chatId=p;}
-function VARIABLES_set_walletAddress(p){walletAddress=p;}
-function VARIABLES_set_password(p){password=p;}
+async function VARIABLES_set_chatId(p){chatId=p;}
+async function VARIABLES_set_walletAddress(p){walletAddress=p;}
+async function VARIABLES_set_password(p){password=p;}
